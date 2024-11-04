49d0c32f4c3a7b61bd8f8124a22b38d10bdd958c
mouhamedjouini <mouhadje@gmail.com>
2024-11-04 20:57:30 +0100

    add commande

--- src/app/Menu/list-menu-user/list-menu-user.component.ts
+++ src/app/Menu/list-menu-user/list-menu-user.component.ts
@@ -1,27 +1,62 @@
 import { Component } from '@angular/core';
 import { Menu } from '../../models/Menu';
 import { MenuService } from '../../services/menu.service';
+import { CommonModule } from '@angular/common';
+import { Commande, Status } from '../../models/Commande';
+import { CommandeService } from '../../services/commande.service';
+import { Route, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-list-menu-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-menu-user.component.html',
   styleUrl: './list-menu-user.component.css'
 })
 export class ListMenuUserComponent {
+ 
+  commande: Commande = {
+    date: new Date(),
+    status: Status.PENDING,
+    userId: 1, // ID de l'utilisateur par défaut
+   
+  };
       Menu: Menu[] = [];
       public url = 'http://localhost:8082/api/images/'
-      constructor(private menuService:MenuService){}
+      constructor(private menuService:MenuService,private commandeService:CommandeService,private router:Router){}
       ngOnInit(): void {
        this.allMenu()
       }
-    
+      AjouterCommande(menuId: Commande["menuId"]) {
+        this.commande.menuId = menuId;
+
+    // Envoyer la commande directement au service sans utiliser formData
+    this.commandeService.Add(this.commande).subscribe({
+      next: (data) => {
+        console.log("Commande ajoutée avec succès :", data);
+        Swal.fire({
+          icon: 'success',
+          title: 'Commande ajoutée!',
+          text: `Le menu  a été ajouté avec succès.`,
+          confirmButtonText: 'OK'
+        }).then(() => {
+          this.router.navigate(['user/MenuC']);
+        });
+      
+      },
+      error: (err) => {
+        console.log("Erreur lors de l'ajout de la commande :", err);
+      }
+        })
+        
+        }
       allMenu(){
         this.menuService.GetAllMenu().subscribe({
           next: (data) => {
             this.Menu=data;
             console.log(this.Menu)
+            
           }, error: (err) => {
             console.log(err)
           }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { DashUserComponent } from './DashboardUser/dash-user/dash-user.component
 import { AddTableComponent } from './DashboardUser/add-table/add-table.component';
 import { ListTableComponent } from './dashboardAdmin/list-table/list-table.component';
 import { ListBComponent } from './DashboardUser/list-b/list-b.component';
+import { ListMenuUserComponent } from './Menu/list-menu-user/list-menu-user.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
@@ -24,7 +25,8 @@ export const routes: Routes = [
     ]},
     { path: 'user', component: DashUserComponent , children:[
         {path: "addT" , component :AddTableComponent},
-        {path:'listb',component:ListBComponent}
+        {path:'listb',component:ListBComponent},
+        {path: 'MenuC',component:ListMenuUserComponent},
        
     ] }
 ];
